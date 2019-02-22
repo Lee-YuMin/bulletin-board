5ad00f8a02613748f4e7ec35f1777fb211ae32e7
LeeYuMin <lym91@hatiolab.com>
2019-02-23 00:13:54 +0900

    삭제 기능 추가 / 다이얼로그 api 추가

---
+++ client/src/api/dialog.js
@@ -0,0 +1,16 @@
+function showDialog(title, message, url, callback) {
+    let msg = {
+        title: title,
+        message: message,
+        confirmCallback: () => {
+          callback ? callback : '';
+
+          if(url && url != ''){
+              window.location.hash = `#/${url}`;
+              window.location.hash = `#/${url}`;
+            }
+        }
+      }
+
+    document.dispatchEvent(new CustomEvent('open-dialog', {detail : msg}));
+};
